656eff1cad70af95d4c675f65ee21cdfb52ae944
Rafik Gouiaa <rafikg@stradigi.ai>
2020-01-31 09:51:32 -0500

    Initial commit

---
+++ model/model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+
+class ImageColorizedModel(object):
+    """
+    Encapsulates the architecture of image colorized network
+    """
+    def __init__(self):
+        pass
+
+    @tf.function
+    def train_one_step(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def evaluate(self):
+        pass
