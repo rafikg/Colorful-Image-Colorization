f49a290d2b1ce6c94a020c9618c8ad503e8d9e48
Rafik Gouiaa <rafikg@stradigi.ai>
2020-02-04 17:22:33 -0500

    Mod: fix an error in the model architecture

--- model/model.py
+++ model/model.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.layers import(Conv2D, UpSampling2D,
-    BatchNormalization)
+import datetime
+from tensorflow.keras.layers import (Conv2D, UpSampling2D,
+                                     BatchNormalization)
 from tensorflow.keras.models import Model
+from utils import ImageReader
+
+# Destroys the current TF graph
+tf.keras.backend.clear_session()
 
 
 class ImageColorizedModel(Model):
@@ -11,25 +16,26 @@ class ImageColorizedModel(Model):
     """
 
     def __init__(self, loss_object, optimizer, train_loss,
-                 name='ColorfulImage'):
+                 num_classes=313, name='ColorfulImage', is_training=True):
         super().__init__(name=name)
         self.loss_object = loss_object
         self.optimizer = optimizer
         self.train_loss = train_loss
+        self.is_training = is_training
 
         # block1
         self.conv1_1 = Conv2D(filters=64, kernel_size=3, strides=1,
                               dilation_rate=1, padding='same')
         self.conv1_2 = Conv2D(filters=64, kernel_size=3, strides=2,
                               dilation_rate=1, padding='same')
-        self.bn1 = BatchNormalization()
+        self.bn1 = BatchNormalization(trainable=self.is_training)
 
         # block2
         self.conv2_1 = Conv2D(filters=128, kernel_size=3, strides=1,
                               dilation_rate=1, padding='same')
         self.conv2_2 = Conv2D(filters=128, kernel_size=3, strides=2,
                               dilation_rate=1, padding='same')
-        self.bn2 = BatchNormalization()
+        self.bn2 = BatchNormalization(trainable=self.is_training)
 
         # block3
         self.conv3_1 = Conv2D(filters=256, kernel_size=3, strides=1,
@@ -38,7 +44,7 @@ class ImageColorizedModel(Model):
                               dilation_rate=1, padding='same')
         self.conv3_3 = Conv2D(filters=256, kernel_size=3, strides=2,
                               dilation_rate=1, padding='same')
-        self.bn3 = BatchNormalization()
+        self.bn3 = BatchNormalization(trainable=self.is_training)
 
         # block4
         self.conv4_1 = Conv2D(filters=512, kernel_size=3, strides=1,
@@ -47,7 +53,7 @@ class ImageColorizedModel(Model):
                               dilation_rate=1, padding='same')
         self.conv4_3 = Conv2D(filters=512, kernel_size=3, strides=1,
                               dilation_rate=1, padding='same')
-        self.bn4 = BatchNormalization()
+        self.bn4 = BatchNormalization(trainable=self.is_training)
 
         # block5
         self.conv5_1 = Conv2D(filters=512, kernel_size=3, strides=1,
@@ -56,7 +62,7 @@ class ImageColorizedModel(Model):
                               dilation_rate=2, padding='same')
         self.conv5_3 = Conv2D(filters=512, kernel_size=3, strides=1,
                               dilation_rate=2, padding='same')
-        self.bn5 = BatchNormalization()
+        self.bn5 = BatchNormalization(trainable=self.is_training)
 
         # block6
         self.conv6_1 = Conv2D(filters=512, kernel_size=3, strides=1,
@@ -65,7 +71,7 @@ class ImageColorizedModel(Model):
                               dilation_rate=2, padding='same')
         self.conv6_3 = Conv2D(filters=512, kernel_size=3, strides=1,
                               dilation_rate=2, padding='same')
-        self.bn6 = BatchNormalization()
+        self.bn6 = BatchNormalization(trainable=self.is_training)
 
         # block7
         self.conv7_1 = Conv2D(filters=256, kernel_size=3, strides=1,
@@ -74,7 +80,7 @@ class ImageColorizedModel(Model):
                               dilation_rate=1, padding='same')
         self.conv7_3 = Conv2D(filters=256, kernel_size=3, strides=1,
                               dilation_rate=1, padding='same')
-        self.bn7 = BatchNormalization()
+        self.bn7 = BatchNormalization(trainable=self.is_training)
 
         # block 8
         self.up_samp = UpSampling2D(size=(2, 2))
@@ -84,7 +90,10 @@ class ImageColorizedModel(Model):
                               dilation_rate=1, padding='same')
         self.conv8_3 = Conv2D(filters=128, kernel_size=3, strides=1,
                               dilation_rate=1, padding='same')
-        self.bn8 = BatchNormalization()
+        self.bn8 = BatchNormalization(trainable=self.is_training)
+        self.final_output = Conv2D(num_classes, (1, 1), activation='softmax',
+                             padding='same',
+                             name='pred')
 
     def call(self, x):
         # block1
@@ -133,32 +142,94 @@ class ImageColorizedModel(Model):
         x = self.conv8_2(x)
         x = self.conv8_3(x)
         x = self.bn8(x)
+        x = self.final_output(x)
         return x
 
     @tf.function
-    def train_one_step(self, x):
+    def train_one_step(self, x, y):
+        """
+        train the model for one step
+        Parameters
+        ----------
+        x : tf.Tensor
+        y: tf.Tensor
+
+        Returns
+        -------
+
+        """
         with tf.GradientTape as tape:
             output = self.__call__(x)
-            loss = self.loss_object(x, output)
-            loss += self.losses + loss
+            loss = self.loss_object(y, output)
         gradients = tape.gradient(loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_weights))
         self.train_loss(loss)
 
-    def fit(self):
+    def fit(self, dataset, epochs, train_log_dir):
+        """
+        fit the model on the dataset
+        Parameters
+        ----------
+        dataset: tf.data.Dataset
+            dataset object
+        epochs: int
+            number of epochs
+        train_log_dir: str
+
+        Returns
+        -------
+        self: trained model
+        """
+        train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+        for epoch in range(epochs):
+            for step, (l, ab) in enumerate(dataset):
+                if epoch == 0 and step == 0:
+                    tf.summary.trace_on(graph=True, profiler=True)
+                self.train_one_step(x=l, y=ab)
+                if epoch == 0 and step == 0:
+                    with train_summary_writer.as_default():
+                        tf.summary.trace_export(
+                            name="my_func_trace",
+                            step=0,
+                            profiler_outdir=train_log_dir)
+                    tf.summary.trace_off()
+            template = 'Epoch {}, Loss: {}'
+            print(template.format(epoch + 1, self.train_loss.result()))
+            with train_summary_writer.as_default():
+                tf.summary.scalar('loss', self.train_loss.result(), step=epoch)
+
+            # reset the metric for the next epoch
+            self.train_loss.reset_states()
+
+    def predict(self, x: tf.Tensor):
         pass
 
-    def predict(self):
-        pass
-
-    def evaluate(self):
+    def evaluate(self, x: tf.Tensor, y: tf.Tensor):
         pass
 
 
 if __name__ == '__main__':
-    model = ImageColorizedModel(loss_object=None, optimizer=None,
-                                train_loss=None)
+    BATCH_SIZE = 8
+    EPOCHS = 10
 
-    x = np.random.rand(1, 224, 224, 1)
-    y = model(x)
-    print(y.shape)
+    loss_object = tf.keras.losses.MeanSquaredError(name='mse')
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    optimizer = tf.optimizers.Adam(learning_rate=1e-3, name='ADAM')
+
+    # Set up summary writers to write the summaries to disk
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
+
+    img_reader = ImageReader(
+        img_path='../VOCtrainval_11-May-2012/VOCdevkit/VOC2012/JPEGImages',
+        ext="*.jpg", height=256, width=256, is_training=True,
+        batch_size=BATCH_SIZE,
+        n_workers=8, epochs=EPOCHS)
+    dataset = img_reader.dataset
+
+    model = ImageColorizedModel(loss_object=loss_object, optimizer=optimizer,
+                                train_loss=train_loss, is_training=True)
+    X = np.random.rand(1, 256, 256, 1)
+    y = model(X)
+
+    model.fit(dataset=dataset, epochs=EPOCHS, train_log_dir=train_log_dir)
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .color_convert import lab_to_rgb
 from .color_convert import rgb_to_lab
 from .color_convert import get_ab, get_lightness
-__all__ = [lab_to_rgb, rgb_to_lab, get_ab, get_lightness]
+from .image_reader import ImageReader
+__all__ = [lab_to_rgb, rgb_to_lab, get_ab, get_lightness, ImageReader]
